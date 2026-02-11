1420ff4ce8095d8da69f36964771fd1c0afa8d9f
zizfsl <ziz.fsl@gmail.com>
2026-02-11 20:44:09 +0530

    Day 2: Implemented variables, types, arithmetic and basic formatting

---
+++ month-01-foundations/notes/week-01-notes.md
@@ -0,0 +1,12 @@
+## Day 2 – Variables & Types
+
+### Key Concepts
+- Everything in Python is an object
+- Dynamic typing
+- Importance of understanding types for ML pipelines
+
+### Observations
+Written the basics of python to understand the data types present in Python.
+
+### Mistakes
+Take special care of the syntax error. One of he mistakes I did was commenting out the name and age variable and it was not defined in the f-string.
---
+++ month-01-foundations/python-basics/01_variables_and_types.py
@@ -0,0 +1,34 @@
+# 01_variables_and_types.py
+
+#printing a string
+print("Hello, World!")
+
+# Integer
+age = 25
+print(type(age))
+
+# Float
+salary = 50000.75
+print(type(salary))
+
+# String
+name = "Faisal"
+print(type(name))
+
+# Boolean
+is_engineer = True
+print(type(is_engineer))
+
+# Arithmetic
+x = 10
+y = 3
+print(x + y)
+print(x / y)
+
+# Type casting
+num_str = "100"
+num_int = int(num_str)
+print(num_int + 50)
+
+# f-string
+print(f"My name is {name} and I am {age} years old.")
